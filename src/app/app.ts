b95adacdc321772d6a27a26a454d56bc7c6e0e4e
Lahari_12 <laharijv6@gmail.com>
2025-10-04 14:21:39 +0530

    second commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Student } from './student/student';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Student],
+  imports: [RouterOutlet,Student,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css', 
 styles : ['h1{ background-color : pink  ;}']  
@@ -13,4 +14,15 @@ styles : ['h1{ background-color : pink  ;}']
 })
 export class App {
   protected readonly title = signal('webapp1');
+  showBox = false;
+  
+ showBoxFn(){
+  this.showBox =true
+ }
+
+  hideBoxFn(){
+  this.showBox =false
+ }
+
 }
+
